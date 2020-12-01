add0022a6a163caa5f6a3f2b55619570bdb215ad
Makos <makos4608@gmail.com>
2020-12-01 20:36:18 +0100

    Initialize commit with basic configuration webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devtool: "source-map",
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Pizza",
+      filename: "index.html",
+      template: "./index.html",
+    }),
+  ],
+};
